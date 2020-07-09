d9691834783db66b708e1cb0cc298469d4f9682d
john-rodewald <jnrodewald99@gmail.com>
2020-07-09 19:42:06 +0200

    Updates

---
+++ precache-manifest.fd57d97093ed8fdc77f6e331c7e22f67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf7a399c52a256d07c5587e5ff2a03f",
+    "url": "/CoinMarketRecapPub/index.html"
+  },
+  {
+    "revision": "67c7ea0ac5dbd91a7a31",
+    "url": "/CoinMarketRecapPub/static/css/main.b1c75dfe.chunk.css"
+  },
+  {
+    "revision": "af5331887b11f1c506e1",
+    "url": "/CoinMarketRecapPub/static/js/2.29cf20e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoinMarketRecapPub/static/js/2.29cf20e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c7ea0ac5dbd91a7a31",
+    "url": "/CoinMarketRecapPub/static/js/main.f55a3c69.chunk.js"
+  },
+  {
+    "revision": "28219901f18898bcf1c6",
+    "url": "/CoinMarketRecapPub/static/js/runtime-main.359a964e.js"
+  },
+  {
+    "revision": "dbe8047a404d78aa03046b7860cd3dfa",
+    "url": "/CoinMarketRecapPub/static/media/banner.dbe8047a.png"
+  }
+]);
